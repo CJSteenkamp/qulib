9c5d79023c839e3ac8d255a2cc67ca0f1fe24e84
CJ Steenkamp <joshsteenkamp2407@gmail.com>
2024-09-28 20:57:48 +0200

    up

--- setup.py
+++ setup.py
@@ -15,6 +15,10 @@ setup(
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
+    url="https://github.com/CJSteenkamp/qulib",
+    maintainer='CJ Steenkamp',
+    maintainer_email='joshsteenkamp2407@gmail.com'
 )
+
 # p setup.py sdist bdist_wheel
 # twine upload dist/*
